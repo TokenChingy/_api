1fc97758878c27dc193b337702269e9b80806d15
Jason Lei <tokenchingy@gmail.com>
2018-10-21 13:01:19 +0800

    Added commenting to `src/router/index.js` file.

--- src/router/index.js
+++ src/router/index.js
@@ -8,19 +8,27 @@ import { isJSONString, responseBuilder, requestQueryHandler } from '../helpers';
 // Define and export a function that automatically generates a CRUD API for a particular collection.
 export default function Router(Server, Collection) {
   _.forEach(Collection.value(), (object, key) => {
-    // Endpoint: Find all, find by id, find by filter.
+    // Endpoint: Find all, find by id, and find by filter.
     Server.get(`/${key}:?`, (request, response) => {
+      // Pass request query object as well as reference to the relevant collection.
+      // The result returned will be data queried, and operated on with the relevant data operators.
       const result = requestQueryHandler(request, Collection, key);
 
+      // Iterate through the header object returned by the query handler and set additional headers.
       _.forEach(result.headers, (value, header) => {
         response.set(header, value);
       });
 
+      // Build and send the response object.
       response.json(responseBuilder(200, request, result.data));
     });
 
     // Endpoint: Create one.
     Server.post(`/${key}/create`, (request, response) => {
+      // Get a handle on the correct collection.
+      // Insert the new document JSON object.
+      // Write it to disk.
+      // After write; build and send a response object.
       Collection.get(key)
         .insert(request.body)
         .write()
@@ -32,6 +40,11 @@ export default function Router(Server, Collection) {
 
     // Endpoint: Update by id or update by filter.
     Server.post(`/${key}/update:?`, (request, response) => {
+      // Check if there is an _id query in the request query object.
+      // If there is, then update the document/documents by id.
+      // If there isn't an _id query, then check if there is a _filter query.
+      // If there is a _filter query, update by filter.
+      // Else, return not found.
       if (!_.isEmpty(request.query._id)) {
         Collection.get(key)
           .updateById(request.query._id, request.body)
@@ -53,12 +66,17 @@ export default function Router(Server, Collection) {
             throw new Error(error);
           });
       } else {
-        response.json(responseBuilder(404, request));
+        response.json(responseBuilder(400, request, {}, { error: 'bad request' }));
       }
     });
 
     // Endpoint: Remove by id or remove by filter.
     Server.post(`/${key}/remove:?`, (request, response) => {
+      // Check if there is an _id query in the request query object.
+      // If there is, then remove the document/documents by id.
+      // If there isn't an _id query, then check if there is a _filter query.
+      // If there is a _filter query, remove by filter.
+      // Else, return not found.
       if (!_.isEmpty(request.query._id)) {
         Collection.get(key)
           .removeById(request.query._id, request.body)
@@ -80,7 +98,7 @@ export default function Router(Server, Collection) {
             throw new Error(error);
           });
       } else {
-        response.json(responseBuilder(404, request));
+        response.json(responseBuilder(400, request, {}, { error: 'bad request' }));
       }
     });
   });
